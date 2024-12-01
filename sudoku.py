10ac16b48412bce486466fb055114284856f8c47
Raphael Sisso <sisso.raphael1@gmail.com>
2024-12-01 14:07:33 +0100

    Done with Sudoku checker

--- sudoku.py
+++ sudoku.py
@@ -1,5 +1,18 @@
 # pylint: disable=missing-docstring
 
-
 def sudoku_validator(grid):
-    pass  # YOUR CODE HERE
+    for i in range(9):
+        if sorted([grid[i][j] for j in range(9)]) != list(range(1, 10)):
+            return False
+
+    for j in range(9):
+        if sorted([grid[i][j] for i in range(9)]) != list(range(1, 10)):
+            return False
+
+    for k in range(3):
+        for h in range(3):
+            if sorted([grid[i][j] for i in range(3*k, 3*k+3)
+                for j in range(3*h, 3*h+3)]) != list(range(1, 10)):
+                return False
+
+    return True
